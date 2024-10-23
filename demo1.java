d8f8aa7ef712a39d806081ab4d0042df68096d4c
nehal bhosale <nehalbhosale0346@gmail.com>
2024-10-23 16:07:15 +0530

    sout method for demo

--- demo1.java
+++ demo1.java
@@ -4,5 +4,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("1235654848494");
         System.out.println("hello");
+        System.out.println("this is demo 1");
     }
 }
